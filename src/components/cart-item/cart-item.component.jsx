668b1d864b3dc9b9023262b0bd1c23a7554690e9
fahim4440 <ahmadul244@gmail.com>
2020-05-21 12:47:00 +0600

    added reselect and cart selector and replaced cart icon and dropdown by reselector

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -6,7 +6,7 @@ const CartItem = ({ item: { name, imageUrl, quantity, price } }) => (
         <img src={imageUrl} alt='item' />
         <div className='item-details'>
             <span className='name'>{name}</span>
-            <span className='price'>{quantity} x {price}</span>
+            <span className='price'>{quantity} x ${price}</span>
         </div>
     </div>
 )
